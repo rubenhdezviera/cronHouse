ca49a4aa9c85e5199bb9baaa3bc34c0f081bbfef
unknown <joaquin.ocon.ojeda@accenture.com>
2018-02-17 12:11:59 +0000

    fix scrapIdealista empty function

--- cronIdealista.js
+++ cronIdealista.js
@@ -13,6 +13,7 @@ var FILE_CONFIG_JOBS = './config_jobs.json'; // File holding array of jobs
 /* GLOBAL VARS */
 var IDEALISTA_BASE_URL = 'https://www.idealista.com';
 var EMAIL_CONFIG = jsonfile.readFileSync(EMAIL_CONFIG_FILE);
+var FILE_EMAIL_TEMPLATE = 'email_template.html';
 var CONFIG_JOBS = jsonfile.readFileSync(FILE_CONFIG_JOBS);
 var CHROME_UA = { 'User-Agent': 'User-Agent: Mozilla/5.0 (Macintosh; Intel Mac OS X 10_10_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.113 Safari/537.36' };
 
@@ -20,111 +21,113 @@ var transporter = nodemailer.createTransport({
   service: EMAIL_CONFIG.mail.service,
   auth: EMAIL_CONFIG.mail.auth
 });
-
 /* /GLOBAL VARS */
 
 //Run each 4 minutes
-new CronJob('0 */4 * * * *', function () {
+new CronJob('0 */2 * * * *', function () {
 
   CONFIG_JOBS.forEach(function(JOB) {
-    console.log('\n\n' + JOB.name + ' JOB started @ ' + new Date() + '\n\nScrapping...');
+    console.log('\n\n' + JOB.name + ': Started scrapping @ ' + new Date());
     JOB['first_run'] = false;
     JOB['num_pages'] = 0;
     JOB['new_items'] = [];
-
-    var request_options = {
+    JOB['request_options'] = {
       url: JOB.init_url,
       transform: function (body) {
         return cheerio.load(body);
       },
       headers: CHROME_UA
     };
-
-    request(request_options).then(function ($) {
-      var items;
-      try {
-        items = jsonfile.readFileSync(JOB.output_json_file);
-      } catch (error) {
-        console.log('\n\nNo existe fichero ' + JOB.output_json_file + ' -> Se creará uno nuevo');
-        items = {};
-        JOB.first_run = true;
-      }
-
-      $('article').each(function (i) {
-        var item = {};
-        item['id_idealista'] = $(this).children('.item').attr('data-adid');
-        item['title'] = $(this).find('.item-link').text();
-        item['link'] = IDEALISTA_BASE_URL + $(this).find('.item-link').attr('href');
-        item['price'] = $(this).find('.item-price').text();
-        item['price'] = item['price'].substr(0, item['price'].search('€/mes')).replace('.', '');
-        item['price'] = item['price'] != '' ? parseInt(item['price']) : 0;
-        item['description'] = $(this).find('.item-description').text();
-        item['num_habs'] = $(this).find('.item-detail').filter(function () {
-          return $(this).html().search('<small>hab.</small>') > -1;
-        }).text().substr(0, 1);
-        item['num_habs'] = item['num_habs'] != '' ? parseInt(item['num_habs']) : 0;
-        item['m2'] = $(this).find('.item-detail').filter(function () {
-          return $(this).html().search('<small>m&#xB2;</small>') > -1;
-        }).text();
-        item['m2'] = item['m2'] != '' ? parseInt(item['m2'].substr(0, item['m2'].indexOf(' '))) : 0;
-        item['date_added'] = new Date().getTime();
-
-        if (!items[item['id_idealista']] && item['price'] > 0 /* && item['price'] < 500 */) {
-          items[item['id_idealista']] = item;
-          JOB.new_items.push(item);
-          //console.log('Added: ' + JSON.stringify(item));
-        }
-      });
-
-      jsonfile.writeFileSync(JOB.output_json_file, items);
-
-      var next_url = $('.icon-arrow-right-after').attr('href');
-      next_url = next_url != undefined ? IDEALISTA_BASE_URL + next_url : '';
-
-      if (next_url != '' && JOB.num_pages < JOB.max_pages) {
-        scrapIdealista(next_url);
-
-      } else {
-        var total_items = Object.keys(items).length;
-        var total_new_items = JOB.new_items.length;
-
-        JOB.new_items.forEach(function (item) {
-          var destinataries = [];
-          JOB.destinataries.forEach(function (user) {
-            var filters = user.filters;
-            var passes = true;
-            for (var property in filters) {
-              if (filters.hasOwnProperty(property)) {
-                if (passes) {
-                  passes = eval(item[property] + filters[property]);
-                }
-              }
-            }
-            if (passes) {
-              destinataries.push(user.email);
-            }
-          });
-
-          if (destinataries.length > 0 && !JOB.first_run) {
-            transporter.sendMail({
-              from: EMAIL_CONFIG.mail.auth.user,
-              to: destinataries.toString(),
-              subject: JOB.name + ' - ' + item.price + ' €/mes | ' + item.title,
-              html: loadEmailTemplate(item)
-            }, function (err, info) {
-              if (err) { console.log(err); }
-            });
-          }
-        });
-        console.log('\n\nDone scrapping ' + JOB.name + ' @ ' + new Date() + '\n\nTotal Items Added: ' + total_new_items + '\n\nTotal Items: ' + total_items);
-      }
-    });
+    scrapIdealista(JOB);
   });
 
 }, null, true, 'Atlantic/Canary');
 
+function scrapIdealista(JOB) {
+  request(JOB.request_options).then(function ($) {
+    var items;
+    try {
+      items = jsonfile.readFileSync(JOB.output_json_file);
+    } catch (error) {
+      console.log('\n' + JOB.name + ': No existe fichero ' + JOB.output_json_file + ' -> Se creará uno nuevo');
+      items = {};
+      JOB.first_run = true;
+    }
+
+    $('article').each(function (i) {
+      var item = {};
+      item['id_idealista'] = $(this).children('.item').attr('data-adid');
+      item['title'] = $(this).find('.item-link').text();
+      item['link'] = IDEALISTA_BASE_URL + $(this).find('.item-link').attr('href');
+      item['price'] = $(this).find('.item-price').text();
+      item['price'] = item['price'].substr(0, item['price'].search('€/mes')).replace('.', '');
+      item['price'] = item['price'] != '' ? parseInt(item['price']) : 0;
+      item['description'] = $(this).find('.item-description').text();
+      item['num_habs'] = $(this).find('.item-detail').filter(function () {
+        return $(this).html().search('<small>hab.</small>') > -1;
+      }).text().substr(0, 1);
+      item['num_habs'] = item['num_habs'] != '' ? parseInt(item['num_habs']) : 0;
+      item['m2'] = $(this).find('.item-detail').filter(function () {
+        return $(this).html().search('<small>m&#xB2;</small>') > -1;
+      }).text();
+      item['m2'] = item['m2'] != '' ? parseInt(item['m2'].substr(0, item['m2'].indexOf(' '))) : 0;
+      item['date_added'] = new Date().getTime();
+
+      if (!items[item['id_idealista']] && item['price'] > 0 /* && item['price'] < 500 */) {
+        items[item['id_idealista']] = item;
+        JOB.new_items.push(item);
+        //console.log('Added: ' + JSON.stringify(item));
+      }
+    });
+
+    jsonfile.writeFileSync(JOB.output_json_file, items);
+
+    var next_url = $('.icon-arrow-right-after').attr('href');
+    next_url = next_url != undefined ? IDEALISTA_BASE_URL + next_url : '';
+
+    if (next_url != '' && ++JOB.num_pages < JOB.max_pages) {
+      JOB.request_options.url = next_url;
+      scrapIdealista(JOB);
+
+    } else {
+      var total_items = Object.keys(items).length;
+      var total_new_items = JOB.new_items.length;
+
+      JOB.new_items.forEach(function (item) {
+        var destinataries = [];
+        JOB.destinataries.forEach(function (user) {
+          var filters = user.filters;
+          var passes = true;
+          for (var property in filters) {
+            if (filters.hasOwnProperty(property)) {
+              if (passes) {
+                passes = eval(item[property] + filters[property]);
+              }
+            }
+          }
+          if (passes) {
+            destinataries.push(user.email);
+          }
+        });
+
+        if (destinataries.length > 0 && !JOB.first_run) {
+          transporter.sendMail({
+            from: EMAIL_CONFIG.mail.auth.user,
+            to: destinataries.toString(),
+            subject: JOB.name + ' - ' + item.price + ' €/mes | ' + item.title,
+            html: loadEmailTemplate(item)
+          }, function (err, info) {
+            if (err) { console.log(err); }
+          });
+        }
+      });
+      console.log('\n' + JOB.name + ': Done scrapping @ ' + new Date() + '\nScrapped pages: ' + JOB.num_pages + '\nTotal Items Added: ' + total_new_items + '\nTotal Items: ' + total_items);
+    }
+  });  
+}
+
 function loadEmailTemplate(item) {
-  var template = fs.readFileSync('email_template.html', "utf8");
+  var template = fs.readFileSync(FILE_EMAIL_TEMPLATE, "utf8");
   for (var property in item) {
     if (item.hasOwnProperty(property)) {
       template = template.replace(new RegExp('\{\{' + property + '\}\}', 'g'), item[property]);
@@ -132,8 +135,3 @@ function loadEmailTemplate(item) {
   }
   return template;
 };
-
-function scrapIdealista(init_url) {
-
-  
-}
